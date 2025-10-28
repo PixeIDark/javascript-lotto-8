2529f1c0b1193416918c5f09832b268e935a0795
PixeIDark <kiki7951@naver.com>
2025-10-29 02:53:05 +0900

    feat: 전체 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,7 @@
 // 제공된 Lotto 클래스를 사용하여 구현해야 한다.
 // Lotto에 numbers 이외의 필드(인스턴스 변수)를 추가할 수 없다.
 // numbers의 접근 제어자인 #은 변경할 수 없다.
-// Lotto의 패키지를 변경할 수 있다.
+// Lotto의 패키지를 변경할 수 있다. => 파일 위치 바꿔도됨
 class Lotto {
   #numbers;
 
@@ -14,6 +14,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  getNumber() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
