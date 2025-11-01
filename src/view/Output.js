cda9a724ac6af5f742f3b79d7c5db39157cbea54
PixeIDark <kiki7951@naver.com>
2025-11-02 01:35:10 +0900

    feat: Output 클래스 구현
    - 로또 구매 개수 출력
    - 로또 번호 출력
    - 당첨 통계 결과 출력
    - 수익률 출력
    - 에러 메시지 출력

---
+++ src/view/Output.js
@@ -0,0 +1,34 @@
+import {MissionUtils} from "@woowacourse/mission-utils";
+import {OUTPUT_MESSAGES} from "../constants/message.js";
+import {formatCurrency} from "../utils/formatter.js";
+
+class Output {
+  printCount(count) {
+    MissionUtils.Console.print(OUTPUT_MESSAGES.PURCHASE_COUNT(count));
+  }
+
+  printLottoNumbers(ticketsString) {
+    MissionUtils.Console.print(ticketsString);
+  }
+
+  printResult(rankResults) {
+    MissionUtils.Console.print(OUTPUT_MESSAGES.WIN_STATISTICS);
+    MissionUtils.Console.print(OUTPUT_MESSAGES.SEPARATOR);
+    rankResults.forEach(result => this.#printRankResult(result));
+  }
+
+  #printRankResult({display, amount, count}) {
+    const money = formatCurrency(amount);
+    MissionUtils.Console.print(OUTPUT_MESSAGES.PURCHASE_RESULT(display, money, count));
+  }
+
+  printProfitRate(rate) {
+    MissionUtils.Console.print(OUTPUT_MESSAGES.PROFIT_RATE(rate));
+  }
+
+  printError(message) {
+    MissionUtils.Console.print(message);
+  }
+}
+
+export default Output;
